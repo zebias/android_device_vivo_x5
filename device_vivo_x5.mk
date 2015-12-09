63a0997af7050d4c5fbe01a8a439149f59baf9bb
zebias <729976854@qq.com>
2015-12-09 14:33:33 +0800

    mtk6752 cwm

--- device_vivo_x5.mk
+++ device_vivo_x5.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 $(call inherit-product-if-exists, vendor/bbk/vivo_x5/vivo_x5-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/bbk/vivo_x5/overlay
-
+LOCAL_PATH := device/bbk/vivo_x5
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/bbk/vivo_x5/kernel
@@ -15,10 +15,16 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/fstab.mt6752:root/fstab.mt6752
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_vivo_x5
 PRODUCT_DEVICE := vivo_x5
+
+# recovery
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/recovery/file_contexts:root/file_contexts \
+	$(LOCAL_PATH)/recovery/ueventd.rc:root/ueventd.rc
